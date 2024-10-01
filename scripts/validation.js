1cd293a174858397c01c560de6cb67543e9287f1
Kyle Smithgall <kyle.smithgall2016@gmail.com>
2024-10-01 16:21:28 -0700

    feat: add enableValidation function - prevent default submit behavior - add style for disabled save/create button

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,11 +1,22 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-enableValidation({
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+  });
+}
+
+const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__form-input",
   submitButtonSelector: ".modal__form-button",
-  inactiveButtonClass: "popup__button_disabled",
+  inactiveButtonClass: ".modal__form-button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
-});
+};
+
+enableValidation(config);
